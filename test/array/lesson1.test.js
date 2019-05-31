05cc70509d03b2dba2afebd3602c635dff2c36f4
Yang-yibu <18335834527@163.com>
2019-05-31 10:01:00 +0800

    添加电话号码测试用例

--- test/array/lesson1.test.js
+++ test/array/lesson1.test.js
@@ -7,3 +7,17 @@ test('telComb: 23', () => {
     "cd", "ce", "cf"
   ])
 })
+
+test('telComb:234', () => {
+  expect(telComb('234')).toEqual([
+    'adg', 'adh', 'adi',
+    'aeg', 'aeh', 'aei',
+    'afg', 'afh', 'afi',
+    'bdg', 'bdh', 'bdi',
+    'beg', 'beh', 'bei',
+    'bfg', 'bfh', 'bfi',
+    'cdg', 'cdh', 'cdi',
+    'ceg', 'ceh', 'cei',
+    'cfg', 'cfh', 'cfi'
+  ])
+})
