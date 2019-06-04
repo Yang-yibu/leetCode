be9d346527524e67b0fd6f2e18bc9f798430d5cc
Yang-yibu <18335834527@163.com>
2019-06-04 12:21:32 +0800

    之前冒泡排序最好时间复杂度符合要求

--- code/sort/lesson1-1.js
+++ code/sort/lesson1-1.js
@@ -1,10 +1,10 @@
 
 // 冒泡排序
-export default (arr) => {
-
+let bubbleSort = (arr) => {
     // 控制循环次数
     for (let i = arr.length - 1; i > 0; i--) {
         for (let j = 0; j < i; j++) {
+            console.log(i);
             // 交换位置，借助临时变量
             let tmp = arr[j];
             if (tmp > arr[j + 1]) {
@@ -15,3 +15,30 @@ export default (arr) => {
     }
     return arr
 }
+
+// 最好时间复杂度可为 O(n)
+let bubbleSort1 = (arr) => {
+    let len = arr.length;
+    // 控制次数
+    for (let i = len - 1; i > 0; i--) {
+        let flag = false;
+        for (let j = 0; j < i; j++) {
+            console.log(i);
+            let tmp = arr[j];
+            // 进行过一次交换，说明还可能有需要交换位置
+            // 本轮循环
+            if (tmp > arr[j + 1]) {
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                flag = true;
+            }
+        }
+        if (!flag) break; // 跳出循环
+    }
+    return arr;
+}
+
+export default bubbleSort1;
+// console.log(bubbleSort([13, 19, 16, 1]));
+// console.log('============');
+// console.log(bubbleSort([1, 4, 5, 6, 7]));
