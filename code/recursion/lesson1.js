f55165cab1fe951e5f60a926cc8471551b345af4
Yang-yibu <tiandaochouqinyn@163.com>
2019-06-15 15:22:21 +0800

    M: 修改 IP 地址的规则，使其通过 LeetCode 的测试

--- code/recursion/lesson1.js
+++ code/recursion/lesson1.js
@@ -7,10 +7,24 @@
 let restoreIpAddress = function (str) {
     let r = [];
 
+
+    /**
+     * 检查 IP 片段是否符合规则
+     * @param {string} segment IP 片段
+     * @return {boolean}
+     */
+    let valid = function (segment) {
+        let len = segment.length;
+
+        if (len > 3) return false;
+
+        return segment[0] != '0' ? segment * 1 < 256 : len === 1;
+    }
+
     /**
      * 搜索符合要求的子 IP 串
      * @param {string[]} subIp 符合要求的子 IP 串
-     * @param {*string} subStr 剩余的子字符串
+     * @param {string} subStr 剩余的子字符串
      */
     let search = function (subIp, subStr) {
         if ( subIp.length > 4 ) { return }
@@ -22,7 +36,7 @@ let restoreIpAddress = function (str) {
             for (let i = 0, len = Math.min(3, subStr.length), tmp; i < len; i++) {
                 tmp = subStr.substr(0, i + 1);
                 // ip: [0, 255]
-                if (tmp < 256) {
+                if ( valid(tmp) ) {
                     search(subIp.concat(tmp), subStr.substr(i + 1))
                 }
             }
--- test/recursion/lesson1.test.js
+++ test/recursion/lesson1.test.js
@@ -3,4 +3,6 @@ import recursion from "../../code/recursion/lesson1";
 
 test('recursion: ', () => {
     expect(recursion('25525511135')).toEqual(["255.255.11.135", "255.255.111.35"])
+
+    expect(recursion("010010")).toEqual(["0.10.0.10","0.100.1.0"])
 });
