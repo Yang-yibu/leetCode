66e254655daf1660de8cbfeb5245e2dedd38349f
Yi-Bu <tiandaochouqinyn163.com>
2020-03-09 17:43:22 +0800

    反转单词

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -4,19 +4,22 @@
  * @return {string}
  */
 
-// export default  function (s) {
+// export default function (s) {
 //   return s.split(' ').map(item => {
 //     return item.split('').reverse().join('')
 //   }).join(' ')
 // };
 
-// export default  function (s) {
+// export default function (s) {
 //   return s.split(/\s+/g).map(item => {
 //     return item.split('').reverse().join('')
 //   }).join(' ')
 // };
 
-export default  function (s) {
+// 这种方式可能不太通用
+// 字符串中如果有其他字符的话需要改正则表达式
+export default function reverseWords (s) {
+  console.log(s.match(/[\w']+/g))
   return s.match(/[\w']+/g).map(item => {
     return item.split('').reverse().join('')
   }).join(' ')
--- test/string/lesson1.test.js
+++ test/string/lesson1.test.js
@@ -1,9 +1,9 @@
-import reverseWords from '../../code/string/lesson1';
+import reverseWords from '../../code/string/lesson1'
 
 test('reverseWords：原字符串单个空格', () => {
-  expect(reverseWords("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc");
+  expect(reverseWords("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
 })
 
 test('reverseWords: 原字符串多个空格', () => {
-  expect(reverseWords("Let's take  LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc");
+  expect(reverseWords("Let's take  LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
 })
