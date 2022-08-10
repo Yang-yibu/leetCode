65dffa43aaecc7584356058d4c77f5f8f594df2a
YiBu-PC <yangyn@louddt.com>
2022-08-10 16:59:04 +0800

    SQL 解析：日志功能调整

--- code/sql/parse.js
+++ code/sql/parse.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
 
+function logFn(group = '分组', ...log) {
+  console.group(group + ': ');
+  log.forEach((ele) => {
+    console.log(ele);
+  });
+  console.groupEnd();
+}
+
 /** 条件匹配器 */
 const regCondition = /\{\{((?!\{\{|\}\}).)*\}\}/g;
 /** 可替换去除包裹器 replace $1 */
@@ -23,9 +31,9 @@ const regParameter_s = /\B(\$\{(?:(?!\$\{|\}).)*\})\B/;
  */
 function getSqlPartOfCondition(sql = '') {
   let result;
-  result = sql.split(regCondition_s).filter(i => i);
+  result = sql.split(regCondition_s).filter((i) => i);
 
-  console.log(JSON.stringify(result, null, '  '));
+  logFn('按参数条件切分', sql, JSON.stringify(result, null, '  '));
   return result;
 }
 /**
@@ -33,11 +41,11 @@ function getSqlPartOfCondition(sql = '') {
  * @param {String} sql sql 语句
  * @returns
  */
- function getSqlPartOfParam(sql = '') {
+function getSqlPartOfParam(sql = '') {
   let result;
-  result = sql.split(regParameter_s).filter(i => i);
+  result = sql.split(regParameter_s).filter((i) => i);
 
-  console.log(JSON.stringify(result, null, '  '));
+  logFn('按参数分割', sql, JSON.stringify(result, null, '  '));
   return result;
 }
 
@@ -50,12 +58,12 @@ function getConditionWithParameters(sqlStr = '') {
   let result;
   result = sqlStr.match(regCondition);
   if (result) {
-    result = result.map(item => {
-      return item.replace(regCondition_r, '$1')
-    })
+    result = result.map((item) => {
+      return item.replace(regCondition_r, '$1');
+    });
   }
 
-  console.log(result);
+  logFn('包含自定义参数的条件表达式', sqlStr, result);
   return result;
 }
 
@@ -68,43 +76,52 @@ function getParameter(sqlStr = '') {
   let result;
   result = sqlStr.match(regParameter);
   if (result) {
-    result = result.map(item => {
-      return item.replace(regParameter_r, '$1')
-    })
+    result = result.map((item) => {
+      return item.replace(regParameter_r, '$1');
+    });
   }
 
-  console.log(result);
+  logFn('获取使用参数集合', sqlStr, result);
   return result;
 }
 
 /** SQL 模板 */
 const sqlArr = {
   /** where 条件具有参数 */
-  whereParams:
-    'select * from 基础表_会计管理数据_会计科目表 where 1=1 \n and {{ 会计科目编码 = ${@会计科目编码} }} \n and {{ 会计年度 in ${@会计年度} }}',
+  whereParams: `select * from 基础表_会计管理数据_会计科目表 where 1=1
+    and {{ 会计科目编码 = \${@会计科目编码} }}
+    and {{ 会计年度 in \${@会计年度} }}`,
   /** 查询字段 & where 有参数 */
-  colWhereParams: 'select ${@会计年度}, 会计科目编码, 会计科目名称, 会计年度 from 基础表_会计管理数据_会计科目表 WHERE 1 = 1 and 会计科目编码 = 1001 and {{ 会计年度 = ${ @会计年度 } }} and {{会计 = 会计}}'
+  colWhereParams: `select \${@会计年度}, 会计科目编码, 会计科目名称, 会计年度
+  from 基础表_会计管理数据_会计科目表 WHERE 1 = 1
+  and 会计科目编码 = 1001
+  and {{ 会计年度 = \${ @会计年度 } }}
+  and {{会计 = 会计}}`,
 };
 
-getSqlPartOfCondition(sqlArr.whereParams)
-getSqlPartOfParam(sqlArr.whereParams)
-getConditionWithParameters(sqlArr.whereParams)
-getParameter(sqlArr.whereParams)
+getSqlPartOfCondition(sqlArr.whereParams);
+getSqlPartOfParam(sqlArr.whereParams);
+getConditionWithParameters(sqlArr.whereParams);
+getParameter(sqlArr.whereParams);
 
-console.log("==============================");
-getSqlPartOfCondition(sqlArr.colWhereParams)
-getSqlPartOfParam(sqlArr.colWhereParams)
-getConditionWithParameters(sqlArr.colWhereParams)
-getParameter(sqlArr.colWhereParams)
+getSqlPartOfCondition(sqlArr.colWhereParams);
+getSqlPartOfParam(sqlArr.colWhereParams);
+getConditionWithParameters(sqlArr.colWhereParams);
+getParameter(sqlArr.colWhereParams);
 
 // const s = sqlArr.colWhereParams.replace(regParameter_r, function (match, p1) {
 //   console.log(match, p1);
 
 //   return "'" + p1 + "'"
 // })
-const s = sqlArr.colWhereParams.replace(regParameter_r, "'$1'")
-console.log(s);
-// 'aaa'.replace(regParameter_r, function (match, p1) {
-  // console.log(match, p1);
-// })
 
+function removeTag(sql = '', reg, log = '去除包裹器') {
+  const s = sql.replace(reg, "'$1'");
+  logFn(log, s);
+}
+removeTag(sqlArr.colWhereParams, regCondition_r, '去除条件包裹器')
+removeTag(sqlArr.colWhereParams, regParameter_r, '去除参数包裹器')
+
+// 'aaa'.replace(regParameter_r, function (match, p1) {
+// console.log(match, p1);
+// })
