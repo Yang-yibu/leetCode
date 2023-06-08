6c06ad2a4d5a63adb8c93b348d7b9c2bee824619
杨亚宁-remote <yangyn@louddt.com>
2023-06-08 22:12:51 +0800

    格式化 sheet 数据

---
+++ buiness/spread/datasource.js
@@ -0,0 +1,372 @@
+export const dataTjfxGc = {
+  FX01: {
+    '2': {
+      '1': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_2',
+        dataType: 'number',
+      },
+      '2': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_3',
+        dataType: 'number',
+      },
+      '3': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_4',
+        dataType: 'number',
+      },
+      '4': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_5',
+        dataType: 'number',
+      },
+      '5': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_6',
+        dataType: 'number',
+      },
+      '6': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_7',
+        dataType: 'number',
+      },
+      '7': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_8',
+        dataType: 'number',
+      },
+      '8': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_9',
+        dataType: 'number',
+      },
+      '9': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_10',
+        dataType: 'number',
+      },
+      '10': {
+        tableName: 'FX01',
+        fieldName: 'TJG3_11',
+        dataType: 'number',
+      },
+      '11': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_12',
+        dataType: 'number',
+      },
+      '12': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_13',
+        dataType: 'number',
+      },
+      '13': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_14',
+        dataType: 'number',
+      },
+      '14': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_15',
+        dataType: 'number',
+      },
+      '15': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_16',
+        dataType: 'number',
+      },
+      '16': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_17',
+        dataType: 'number',
+      },
+      '17': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_18',
+        dataType: 'number',
+      },
+      '18': {
+        tableName: 'FX02',
+        fieldName: 'TJG3_19',
+        dataType: 'number',
+      },
+    },
+    '3': {
+      '1': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_2',
+        dataType: 'number',
+      },
+      '2': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_3',
+        dataType: 'number',
+      },
+      '3': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_4',
+        dataType: 'number',
+      },
+      '4': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_5',
+        dataType: 'number',
+      },
+      '5': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_6',
+        dataType: 'number',
+      },
+      '6': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_7',
+        dataType: 'number',
+      },
+      '7': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_8',
+        dataType: 'number',
+      },
+      '8': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_9',
+        dataType: 'number',
+      },
+      '9': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_10',
+        dataType: 'number',
+      },
+      '10': {
+        tableName: 'FX01',
+        fieldName: 'TJG4_11',
+        dataType: 'number',
+      },
+      '11': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_12',
+        dataType: 'number',
+      },
+      '12': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_13',
+        dataType: 'number',
+      },
+      '13': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_14',
+        dataType: 'number',
+      },
+      '14': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_15',
+        dataType: 'number',
+      },
+      '15': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_16',
+        dataType: 'number',
+      },
+      '16': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_17',
+        dataType: 'number',
+      },
+      '17': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_18',
+        dataType: 'number',
+      },
+      '18': {
+        tableName: 'FX02',
+        fieldName: 'TJG4_19',
+        dataType: 'number',
+      },
+    },
+    '4': {
+      '1': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_2',
+        dataType: 'number',
+      },
+      '2': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_3',
+        dataType: 'number',
+      },
+      '3': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_4',
+        dataType: 'number',
+      },
+      '4': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_5',
+        dataType: 'number',
+      },
+      '5': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_6',
+        dataType: 'number',
+      },
+      '6': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_7',
+        dataType: 'number',
+      },
+      '7': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_8',
+        dataType: 'number',
+      },
+      '8': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_9',
+        dataType: 'number',
+      },
+      '9': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_10',
+        dataType: 'number',
+      },
+      '10': {
+        tableName: 'FX01',
+        fieldName: 'TJG5_11',
+        dataType: 'number',
+      },
+      '11': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_12',
+        dataType: 'number',
+      },
+      '12': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_13',
+        dataType: 'number',
+      },
+      '13': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_14',
+        dataType: 'number',
+      },
+      '14': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_15',
+        dataType: 'number',
+      },
+      '15': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_16',
+        dataType: 'number',
+      },
+      '16': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_17',
+        dataType: 'number',
+      },
+      '17': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_18',
+        dataType: 'number',
+      },
+      '18': {
+        tableName: 'FX02',
+        fieldName: 'TJG5_19',
+        dataType: 'number',
+      },
+    },
+    '5': {
+      '1': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_2',
+        dataType: 'number',
+      },
+      '2': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_3',
+        dataType: 'number',
+      },
+      '3': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_4',
+        dataType: 'number',
+      },
+      '4': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_5',
+        dataType: 'number',
+      },
+      '5': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_6',
+        dataType: 'number',
+      },
+      '6': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_7',
+        dataType: 'number',
+      },
+      '7': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_8',
+        dataType: 'number',
+      },
+      '8': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_9',
+        dataType: 'number',
+      },
+      '9': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_10',
+        dataType: 'number',
+      },
+      '10': {
+        tableName: 'FX01',
+        fieldName: 'TJG6_11',
+        dataType: 'number',
+      },
+      '11': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_12',
+        dataType: 'number',
+      },
+      '12': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_13',
+        dataType: 'number',
+      },
+      '13': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_14',
+        dataType: 'number',
+      },
+      '14': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_15',
+        dataType: 'number',
+      },
+      '15': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_16',
+        dataType: 'number',
+      },
+      '16': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_17',
+        dataType: 'number',
+      },
+      '17': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_18',
+        dataType: 'number',
+      },
+      '18': {
+        tableName: 'FX02',
+        fieldName: 'TJG6_19',
+        dataType: 'number',
+      },
+    },
+  },
+};
---
+++ buiness/spread/format.js
@@ -0,0 +1,110 @@
+import { dataTjfxGc } from './datasource';
+
+/**
+ * @typedef CellMetaOri
+ * @property {string} tableName
+ * @property {string} fieldName
+ * @property {string} dataType
+ */
+/**
+ * @typedef CellMeta
+ * @property {string} tableName
+ * @property {string} fieldName
+ * @property {string} dataType
+ * @property {string} sheet
+ * @property {number} posRow
+ * @property {number} posCol
+ */
+
+/**
+ * 获取某行所有单元格
+ * @param {{[colIdx: string]: CellMeta}} rowCells 所有行
+ */
+function getRowCells(rowCells) {
+  const cells = [];
+  Object.keys(rowCells).map((colIdx) => {
+    cells.push(rowCells[colIdx]);
+  });
+  return cells;
+}
+/**
+ * 获取所有行
+ * @param {{[rowIdx: string]: any}} sheet
+ */
+function getSheetRows(sheet) {
+  const rows = [];
+  Object.keys(sheet).map((rowIdx) => {
+    rows.push(sheet[rowIdx]);
+  });
+  return rows;
+}
+/**
+ * 获取搜索 sheet 页
+ * @param {[sheet: string]: any} tpl
+ */
+function getTpmSheets(tpl) {
+  const sheets = [];
+  Object.keys(sheets).map((sheetName) => {
+    sheets.push(tpl[sheetName]);
+  });
+  return sheets;
+}
+
+function getInfoStr(...arr) {
+  return arr.join('-');
+}
+function getInfoArr(...str) {
+  return str.split('-');
+}
+
+/**
+ * @param {dataTjfxGc} data
+ * @param {string} sheetName
+ * @param {string} rowIdx
+ * @param {string} colIdx
+ */
+function getAllCells(data, { limSheetName, limRowIdx, limColIdx } = {}) {
+  let sheetNames = Object.keys(data);
+  if (data[limSheetName]) {
+    sheetNames = [limSheetName];
+  }
+
+  const allCells = [];
+  sheetNames.map((sheetName) => {
+    Object.keys(data[sheetName]).map((rowIdx) => {
+      Object.keys(data[sheetName][rowIdx]).map((colIdx) => {
+        const cell = data[sheetName][rowIdx][colIdx];
+        cell.sheet = sheetName;
+        cell.posRow = rowIdx;
+        cell.posCol = colIdx;
+        allCells.push(cell);
+      });
+    });
+  });
+
+  return allCells;
+}
+
+const allCells = getAllCells(dataTjfxGc);
+console.log(allCells);
+
+/**
+ * 构建 pos 和 数据库表字段映射信息
+ * @param {CellMeta[]} allCells
+ */
+function buildMapPos2TF(allCells) {
+  const mapPos2TF = {};
+  const mapTF2Pos = {};
+  allCells.map((cell) => {
+    const posStr = getInfoStr(cell.posRow, cell.posCol);
+    const tfStr = getInfoStr(cell.tableName, cell.fieldName);
+
+    mapPos2TF[posStr] = [cell.tableName, cell.fieldName];
+    mapTF2Pos[tfStr] = [cell.posRow, cell.posCol];
+  });
+  return { mapPos2TF, mapTF2Pos };
+}
+
+const metaMap = buildMapPos2TF(allCells);
+console.log(metaMap.mapPos2TF);
+console.log(metaMap.mapTF2Pos);
