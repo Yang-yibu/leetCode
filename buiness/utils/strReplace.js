8a6fab50b164fa4a8564f894a1f2b93a9f13aaa6
杨亚宁-remote <yangyn@louddt.com>
2024-06-21 10:58:43 +0800

    莫奈大屏变量替换方法优化

--- buiness/utils/strReplace.js
+++ buiness/utils/strReplace.js
@@ -8,10 +8,39 @@ function strReplace1(str = '', varPool = {}) {
   return s;
 }
 
+// 莫奈大屏变量替换
 export function strReplace(str = '', varPool = {}) {
-  let s = str.replace(/":(\w+?)"/g, function (sub, $1) {
-    return varPool[$1] ? `"${varPool[$1]}"` : sub;
-  });
+  let reg;
+  // TODO: \x00-\xff 表示翻译
+  // eslint-disable-next-line no-control-regex
+  // reg = /:((?:[^\x00-\xff]|[a-zA-Z_$])(?:[^\x00-\xff]|[a-zA-Z0-9_$])*)/g;
+  // 可以解析处理变量名
+  // eslint-disable-next-line no-control-regex
+  reg = /:?((?:[^\x00-\xff]|[a-zA-Z_$])(?:[^\x00-\xff]|[a-zA-Z0-9_$])*)/g;
+  // reg = /":(\w+?)"/g;
 
+  let s = str
+    .replace(reg, function (sub, $1) {
+      // return varPool[$1] ? `"${varPool[$1]}"` : sub;
+      // return varPool[$1] ? `${varPool[$1]}` : sub;
+
+      if (sub.indexOf(':') === -1) {
+        return `"${sub}"`;
+      }
+      return varPool[$1] ? `${varPool[$1]}` : sub;
+    })
+    .replaceAll('""', '"');
+
+  return s;
+}
+
+export function strReplaceJson(str = '', varPool = {}) {
+  var s = strReplace(str, varPool);
+  try {
+    s = JSON.parse(s);
+  } catch (e) {
+    console.log('解析错误：', s);
+    console.log(e.message);
+  }
   return s;
 }
--- buiness/utils/strReplace.test.js
+++ buiness/utils/strReplace.test.js
@@ -1,4 +1,4 @@
-import { strReplace } from './strReplace';
+import { strReplace, strReplaceJson } from './strReplace';
 
 test('执行字符串函数体', () => {
   expect(
@@ -7,4 +7,46 @@ test('执行字符串函数体', () => {
       clType2_yx: '2',
     })
   ).toEqual('{\n"kind": "1",\n"level": "2",\n"orgCode": ":orgCode_yx"\n}');
+});
+
+describe('顺带解析为 JSON', () => {
+  test('解析规范字符串', () => {
+    expect(
+      strReplaceJson(
+        '{\n"kind": ":clType_gcfc",\n"level": ":clType2_yx",\n"orgCode": ":orgCode_yx"\n}',
+        {
+          clType_gcfc: '1',
+          clType2_yx: '2',
+        }
+      )
+    ).toEqual({ kind: '1', level: '2', orgCode: ':orgCode_yx' });
+  });
+
+  test('解析变量名缺少引号', () => {
+    const res = strReplaceJson(
+      '{\n"kind": :clType_gcfc,\n"level": ":clType2_yx",\n"orgCode": ":orgCode_yx"\n}',
+      {
+        clType_gcfc: '1',
+        clType2_yx: '2',
+      }
+    );
+    // expect(res).toEqual({ kind: '1', level: '2', orgCode: ':orgCode_yx' });
+    expect([
+      { kind: '1', level: '2', orgCode: ':orgCode_yx' },
+      { kind: 1, level: '2', orgCode: ':orgCode_yx' },
+      // { kind: 2, level: '2', orgCode: ':orgCode_yx' },
+    ]).toContainEqual(res);
+  });
+
+  test('解析键名缺少引号', () => {
+    expect(
+      strReplaceJson(
+        '{\nkind: ":clType_gcfc",\n"level": ":clType2_yx",\n"orgCode": ":orgCode_yx"\n}',
+        {
+          clType_gcfc: '1',
+          clType2_yx: '2',
+        }
+      )
+    ).toEqual({ kind: '1', level: '2', orgCode: ':orgCode_yx' });
+  });
 });
