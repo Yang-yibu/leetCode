3234a93e5857ca0c6e48998e4fac13fc2e96d94f
杨亚宁-remote <yangyn@louddt.com>
2024-06-21 11:28:03 +0800

    莫奈替换变量修改

--- buiness/utils/monaiVarPoolStrReplace.js
+++ buiness/utils/monaiVarPoolStrReplace.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-control-regex */
 function strReplace1(str = '', varPool = {}) {
   // let s = str.replace(/\\":(\w+?)\\"/g, '$1$1');
   let s = str.replace(/":(\w+?)"/g, function (sub, $1) {
@@ -8,22 +9,21 @@ function strReplace1(str = '', varPool = {}) {
   return s;
 }
 
+// TODO: \x00-\xff 表示翻译
+// reg = /:((?:[^\x00-\xff]|[a-zA-Z_$])(?:[^\x00-\xff]|[a-zA-Z0-9_$])*)/g;
+// 可以解析处理变量名
+// reg = /":(\w+?)"/g;
+
+// return varPool[$1] ? `"${varPool[$1]}"` : sub;
+// return varPool[$1] ? `${varPool[$1]}` : sub;
+
 // 莫奈大屏变量替换
 export function strReplace(str = '', varPool = {}) {
   let reg;
-  // TODO: \x00-\xff 表示翻译
-  // eslint-disable-next-line no-control-regex
-  // reg = /:((?:[^\x00-\xff]|[a-zA-Z_$])(?:[^\x00-\xff]|[a-zA-Z0-9_$])*)/g;
-  // 可以解析处理变量名
-  // eslint-disable-next-line no-control-regex
   reg = /:?((?:[^\x00-\xff]|[a-zA-Z_$])(?:[^\x00-\xff]|[a-zA-Z0-9_$])*)/g;
-  // reg = /":(\w+?)"/g;
 
   let s = str
     .replace(reg, function (sub, $1) {
-      // return varPool[$1] ? `"${varPool[$1]}"` : sub;
-      // return varPool[$1] ? `${varPool[$1]}` : sub;
-
       if (sub.indexOf(':') === -1) {
         return `"${sub}"`;
       }
